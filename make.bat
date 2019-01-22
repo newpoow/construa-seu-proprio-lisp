94654ee360f0d57e6b0dd2f6ccaeb63a223bd554
Ney Pinheiro <ney.ps@hotmail.com>
2019-01-22 17:48:06 -0200

    parsing

--- make.bat
+++ make.bat
@@ -2,5 +2,8 @@
 rem gcc -std=c99 -Wall .\src\hello_world.c -o .\bin\hello_world.exe
 rem .\bin\hello_world.exe
 
-gcc -std=c99 -Wall .\src\prompt.c -o .\bin\prompt.exe
-.\bin\prompt.exe
+rem gcc -std=c99 -Wall .\src\prompt.c -o .\bin\prompt.exe
+rem .\bin\prompt.exe
+
+gcc -std=c99 -Wall .\src\mpc.c .\src\parsing.c -o .\bin\parsing.exe
+.\bin\parsing.exe
