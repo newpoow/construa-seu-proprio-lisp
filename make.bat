58c718c3bd162a6d3ddd6050c7a25b5d89c1501f
Ney Pinheiro <ney.ps@hotmail.com>
2019-02-14 18:54:26 -0200

    error handling

--- make.bat
+++ make.bat
@@ -8,5 +8,8 @@ rem .\bin\prompt.exe
 rem gcc -std=c99 -Wall .\src\mpc.c .\src\parsing.c -o .\bin\parsing.exe
 rem .\bin\parsing.exe
 
-gcc -std=c99 -Wall .\src\mpc.c .\src\evaluation.c -o .\bin\evaluation.exe
-.\bin\evaluation.exe
+rem gcc -std=c99 -Wall .\src\mpc.c .\src\evaluation.c -o .\bin\evaluation.exe
+rem .\bin\evaluation.exe
+
+gcc -std=c99 -Wall .\src\mpc.c .\src\error_handling.c -o .\bin\error_handling.exe
+.\bin\error_handling.exe
