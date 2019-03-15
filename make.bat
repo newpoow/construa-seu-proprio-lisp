ab2defaed3d08733fa26168958939349fa4f1a1e
Ney Pinheiro <ney.ps@hotmail.com>
2019-03-15 18:02:47 -0300

    variables

--- make.bat
+++ make.bat
@@ -17,5 +17,8 @@ rem .\bin\error_handling.exe
 rem gcc -std=c99 -Wall .\src\mpc.c .\src\s_expressions.c -o .\bin\s_expressions.exe
 rem .\bin\s_expressions.exe
 
-gcc -std=c99 -Wall .\src\mpc.c .\src\q_expressions.c -o .\bin\q_expressions.exe
-.\bin\q_expressions.exe
+rem gcc -std=c99 -Wall .\src\mpc.c .\src\q_expressions.c -o .\bin\q_expressions.exe
+rem .\bin\q_expressions.exe
+
+gcc -std=c99 -Wall .\src\mpc.c .\src\variables.c -o .\bin\variables.exe
+.\bin\variables.exe
